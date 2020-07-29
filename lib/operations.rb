cc52c1a5f2c990b00b1e2a97a9028f35b4f53611
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-07-29 02:51:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,15 @@
+require 'pry'
 def unsafe?(speed)
   if speed >= 40 || speed <= 60
-    false
-  else
     true
+  else
+    false
   end
 end
 
+binding.pry
 
-
-def not_safe?(speed)
-	speed > 40 || speed < 60 ? false : true
-end
+# def not_safe?(speed)
+# 	speed >= 40 || speed < 60 ? false : true
+# end
 	
